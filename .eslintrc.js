b30a7fd73e7841a9c2c0c32eeaf882cd05d54103
Mikael Edebro <mikael.edebro@gmail.com>
2018-03-19 14:46:07 +0100

    Add Eslint & eslint-plugin-vue. Also enable this for Poi

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  extends: ['standard', 'plugin:vue/recommended'],
+  rules: {
+    'arrow-parens': 'off',
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'spaced-comment': 'off',
+    'eol-last': 'off',
+    'space-before-function-paren': 'off',
+    'no-multi-spaces': ['error', { ignoreEOLComments: true }],
+    'prefer-promise-reject-errors': 'off',
+
+    // vue-eslint-plugin
+    // https://github.com/vuejs/eslint-plugin-vue
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 3,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+          normal: 'any',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ],
+    'vue/require-default-prop': 'off',
+    'vue/attributes-order': ['off', {}],
+    'vue/attribute-order': ['off', {}],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'always'
+      }
+    ]
+  }
+}
---
+++ poi.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  entry: 'dev/index.js',
+  dist: 'dev/dist',
+  homepage: '/vue-airbnb-style-datepicker/',
+  presets: [require('poi-preset-eslint')({ mode: '*' })]
+}
