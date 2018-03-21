bb8af87f937839035be5951ababc2f87f39c05d9
Mikael Edebro <mikael.edebro@gmail.com>
2018-03-21 20:30:02 +0100

    ClickOutside directive was not working in iOS.

--- src/directives/ClickOutside.js
+++ src/directives/ClickOutside.js
@@ -1,11 +1,13 @@
 export default {
   bind: function(el, binding, vnode) {
     el.event = function(event) {
+      console.log('v-click-outside')
       if (!(el === event.target || el.contains(event.target))) {
         vnode.context[binding.expression](event)
       }
     }
-    document.body.addEventListener('click', el.event)
+    const touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click'
+    document.body.addEventListener(touchEvent, el.event)
   },
   unbind: function(el) {
     document.body.removeEventListener('click', el.event)
