1f556d9b3c73e075ac4b6c5c7b9d4dfc34123b78
GiorgiosaudOld <jorgelsaud@gmail.com>
2018-05-17 11:48:58 -0400

    fix class

--- src/components/AirbnbStyleMonthpicker.vue
+++ src/components/AirbnbStyleMonthpicker.vue
@@ -15,12 +15,12 @@
         <h3>{{ mobileHeader }}</h3>
       </div>
       <div class="asd__datepicker-header">
-        <div class="asd__change-month-button asd__change-month-button--previous">
+        <div class="asd__change-year-button asd__change-year-button--previous">
           <button @click="previousYear" type="button">
             <svg viewBox="0 0 1000 1000"><path d="M336.2 274.5l-210.1 210h805.4c13 0 23 10 23 23s-10 23-23 23H126.1l210.1 210.1c11 11 11 21 0 32-5 5-10 7-16 7s-11-2-16-7l-249.1-249c-11-11-11-21 0-32l249.1-249.1c21-21.1 53 10.9 32 32z" /></svg>
           </button>
         </div>
-        <div class="asd__change-month-button asd__change-month-button--next">
+        <div class="asd__change-year-button asd__change-year-button--next">
           <button @click="nextYear" type="button">
             <svg viewBox="0 0 1000 1000"><path d="M694.4 242.4l249.1 249.1c11 11 11 21 0 32L694.4 772.7c-5 5-10 7-16 7s-11-2-16-7c-11-11-11-21 0-32l210.1-210.1H67.1c-13 0-23-10-23-23s10-23 23-23h805.4L662.4 274.5c-21-21.1 11-53.1 32-32.1z" /></svg>
           </button>
@@ -931,7 +931,7 @@ $transition-time: 0.3s;
     position: relative;
   }
 
-  &__change-month-button {
+  &__change-year-button {
     position: absolute;
     top: 12px;
     z-index: 10;
