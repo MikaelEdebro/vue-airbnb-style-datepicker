0808d638d33ea9890b87ff8feee0517922df3f45
Mikael Edebro <mikael.edebro@gmail.com>
2018-07-23 16:47:59 +0200

    Removing failing test (travis integration check)

--- src/components/__tests__/AirbnbStyleDatepicker.spec.js
+++ src/components/__tests__/AirbnbStyleDatepicker.spec.js
@@ -298,7 +298,6 @@ describe('AirbnbStyleDatepicker', () => {
       wrapper.vm.triggerElement.dispatchEvent(new Event('focus'))
       wrapper.update()
       expect(wrapper.findAll('.asd__selected-date-one').length).toBe(0)
-      expect(false).toBe(true)
     })
   })
 })
